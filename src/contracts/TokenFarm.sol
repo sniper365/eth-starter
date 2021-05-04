174646846988b089e4e12682e921f24688b5519d
sniper365 <devtops2050@gmail.com>
2021-05-03 22:40:40 -0500

    create stakeTokens function

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -8,9 +8,31 @@ contract TokenFarm {
     AlexxToken public alexxToken;
     DaiToken public daiToken;
 
+    address[] public stakers;
+    mapping(address => uint) public stakingBalance;
+    mapping(address => bool) public hasStaked;
+    mapping(address => bool) public isStaking;
+
     constructor(AlexxToken _alexxToken, DaiToken _daiToken) public {
         alexxToken = _alexxToken;
         daiToken = _daiToken;
     }
+    //1. Stakes tokens (deposits dai tokens)
+    function stakeTokens(uint _amount) public {
+        
+        //transfer mock dai tokens to this contract for staking
+        daiToken.transferFrom(msg.sender, address(this), _amount);
 
+        //update staking balance
+        stakingBalance[msg.sender] = stakingBalance[msg.sender]+_amount;
+
+        //add users to stakers array only if they havent staked
+        if(!hasStaked[msg.sender]){
+            stakers.push(msg.sender);
+        }
+
+        //update staker status
+        hasStaked[msg.sender] = true;
+        isStaking[msg.sender] = true;
+    }
 }
